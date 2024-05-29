1fe72abad7bf96612fd7cc256f67d11c53123631
jtmer276 <jtmerrill276@gmail.com>
2024-05-29 14:29:01 -0400

    Update ApplicationRobot.java

--- ApplicationRobot.java
+++ ApplicationRobot.java
@@ -1,11 +1,18 @@
 import java.awt.*;
 import java.awt.event.InputEvent;
+//import java.security.Key;
 
 public class ApplicationRobot{
     public static void main(String[] args) throws Exception{
         Robot jules = new Robot();
         //jules.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-        jules.mouseMove(0, 1600);
-        jules.mousePress(InputEvent.ALT_DOWN_MASK);
+        jules.mouseMove(0, 1500);
+        jules.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        jules.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        jules.delay(500);
+        jules.mouseMove(100, 970);
+        jules.delay(500);
+        jules.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        jules.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
     }
 }
